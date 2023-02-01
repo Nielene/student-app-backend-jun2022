edea404610566b6db42e2e33d3b08dadabf952cd
Nielene Thomas <thomasnielene@gmail.com>
2023-02-01 07:21:58 -0500

    add get all and get one on studentsController.js

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const express = require("express");
 // initialize app
 const app = express();
 
+// set up routes/controllers; same name as the variable itself:
+const studentsController = require("./controllers/studentsController");
+
+// when s'thing goes to /9000/student app, use studentController
+app.use("/students", studentsController);
+
 // route
 app.get("/", (request, response) => {
   response.send("Hello world!");
---
+++ controllers/studentsController.js
@@ -0,0 +1,46 @@
+// same as app.js setup
+const express = require("express");
+const { response } = require("../app");
+const controller = express.Router();
+
+const studentData = require("../studentData.json");
+
+// controller.get('/', (request, response) => {
+//     response.send("Hello from the students controller")
+// })
+
+// controller.get("/", (request, response) => {
+//   response.json({ hello: "world" });
+// });
+
+controller.get("/", (request, response) => {
+  response.json(studentData);
+});
+
+controller.get("/:id", (request, response) => {
+  try {
+    const studentId = request.params.id;
+
+    if (!/[0-9]/.test(studentId)) {
+      response.send("Student ID must be a number.");
+      return;
+    }
+
+    const singleStudent = studentData.students.find((student) => {
+      return student.id === studentId;
+    });
+    //   response.json(studentData.students.id);
+    console.log(singleStudent);
+
+    if (singleStudent) {
+      response.json(singleStudent);
+    } else {
+      response.send("Student not found.");
+    }
+  } catch (err) {
+    response.status(500).send("An error occurred.");
+  }
+});
+
+// sending it back to app.js
+module.exports = controller;
